b2d5b9877d26b06ebacf1f22b9a293e373dcb45f
EgoRedMC <egoredmc@gmail.com>
2018-05-05 14:55:34 +0300

    Work on plugin action was started.

---
+++ src/OpenDocumentation.java
@@ -0,0 +1,12 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class OpenDocumentation extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+//        System.out.println("click");
+        
+    }
+}
