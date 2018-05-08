c6f5eaf0449d0d23ffe1d2c9fbce8e5b3d6c1df1
EgoRedMC <egoredmc@gmail.com>
2018-05-08 12:36:52 +0300

    Got into IntelliJ Platform action api, main logic method was started.

--- src/OpenDocumentation.java
+++ src/OpenDocumentation.java
@@ -1,12 +1,32 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.VisualPosition;
+import com.intellij.openapi.ui.Messages;
 
 public class OpenDocumentation extends AnAction {
 
     @Override
+    public void update(AnActionEvent e) {
+        Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        for (Caret c : editor.getCaretModel().getAllCarets()) {
+            c.selectWordAtCaret(false);
+            makeMainStuff(c.getSelectedText());
+            c.removeSelection();
+        }
+        System.out.println(2);
+    }
+
+    //main logic
+    private void makeMainStuff(String word) {
+
+    }
+
+    //happens after action performing
+    @Override
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
-//        System.out.println("click");
-        
     }
 }
