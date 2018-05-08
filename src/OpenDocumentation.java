94dce8b71b1053252685874418512084a1e3f6a5
EgoRedMC <egoredmc@gmail.com>
2018-05-08 22:54:01 +0300

    Imports reading was realized.

--- src/OpenDocumentation.java
+++ src/OpenDocumentation.java
@@ -2,22 +2,98 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Caret;
-import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.VisualPosition;
-import com.intellij.openapi.ui.Messages;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class OpenDocumentation extends AnAction {
+
+    private static ArrayList<String> simpleImports;
+    private static ArrayList<String> multiImports;
 
     @Override
     public void update(AnActionEvent e) {
         Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
-        for (Caret c : editor.getCaretModel().getAllCarets()) {
-            c.selectWordAtCaret(false);
-            makeMainStuff(c.getSelectedText());
-            c.removeSelection();
+        sortImports(readImports(editor.getDocument()));
+        //
+//        for (Caret c : editor.getCaretModel().getAllCarets()) {
+//            c.selectWordAtCaret(false);
+//            makeMainStuff(c.getSelectedText());
+//            c.removeSelection();
+//        }
+    }
+
+    /**
+     * splits imports into simple (java.util.ArrayList) and multi (java.io.*)
+     *
+     * @param imports ArrayList of imports represented as String's
+     */
+    private void sortImports(ArrayList<String> imports) {
+        simpleImports = new ArrayList<>(imports.size() / 4);
+        multiImports = new ArrayList<>(imports.size() / 4);
+        for (String imp : imports) {
+            if (imp.charAt(imp.length() - 1) == '*')
+                multiImports.add(imp);
+            else
+                simpleImports.add(imp);
         }
-        System.out.println(2);
+    }
+
+    /**
+     * reads all the imports in input Document
+     *
+     * @return ArrayList which contains all imports represented as String's
+     */
+    private ArrayList<String> readImports(Document document) {
+        ArrayList<String> imports = new ArrayList<>(16);
+        imports.add("java.lang.*");
+        //
+        String text = document.getText();
+        StringTokenizer tokenizer = new StringTokenizer(text, ";");
+        while (tokenizer.hasMoreTokens()) {
+            String line = tokenizer.nextToken();
+            if (!line.contains("import ")) {
+                if (containsNonWhitespaces(line))
+                    break;
+                else
+                    continue;
+            }
+            char[] word = new char[line.length() - "import ".length()];
+            int index = 0;
+
+            for (int i = "import ".length() + skipWhitespaces(line); i < line.length(); i++) {
+                if (line.charAt(i) == ' ') continue;
+                word[index++] = line.charAt(i);
+            }
+            imports.add(new String(word, 0, index));
+        }
+        return imports;
+    }
+
+    /**
+     * @return number of whitespaces at the beginning of input String
+     */
+    private int skipWhitespaces(String line) {
+        for (int i = 0; i < line.length(); i++) {
+            if (!Character.isWhitespace(line.charAt(i))) return i;
+        }
+        return -1;
+    }
+
+    /**
+     * tells if String contains any non-whitespace character
+     *
+     * @param line String to check
+     * @return true if input is null or contains non-whitespace characters
+     * otherwise false
+     */
+    private boolean containsNonWhitespaces(String line) {
+        for (int i = 0; i < line.length(); i++) {
+            if (!Character.isWhitespace(line.charAt(i))) return true;
+        }
+        return false;
     }
 
     //main logic
