1884b5efb4e38eab24fffef3b669daae8e303786
NewUserKK <newuser9929@gmail.com>
2018-05-09 15:01:13 +0300

    Simplify some code

--- src/OpenAllClasses.java
+++ src/OpenAllClasses.java
@@ -2,7 +2,9 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
 import java.awt.*;
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 public class OpenAllClasses extends AnAction {
 
@@ -10,7 +12,7 @@ public class OpenAllClasses extends AnAction {
     public void actionPerformed(AnActionEvent e) {
         try {
             Desktop.getDesktop().browse(new URI("https://docs.oracle.com/javase/7/docs/api/allclasses-noframe.html"));
-        } catch (Exception ignored) {
+        } catch (URISyntaxException | IOException ignored) {
         }
     }
 }
